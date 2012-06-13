48f01ab1dccef01c7a87b1db91b1d0347a2d29ed
johnbaums <john.baumgartner@live.com>
2012-06-13 16:04:30 +1000

    bugfix

--- R/jagsresults.R
+++ R/jagsresults.R
@@ -4,15 +4,15 @@ jagsresults <-
       rows <- unique(c(sapply(params, function(z) {
         if (exact) {
           grep(paste('^', z, '$', sep=''), 
-            gsub('\\[[0-9]+\\]', '', row.names(jagsfit$BUGSoutput$summary)), 
+            gsub('\\[[0-9]+\\]', '', row.names(x$BUGSoutput$summary)), 
                invert=invert)
         } else {
-          grep(z, row.names(jagsfit$BUGSoutput$summary), invert=invert)
+          grep(z, row.names(x$BUGSoutput$summary), invert=invert)
         }
       }), recursive=TRUE))
     } else {
       rows <- grep(params, gsub('\\[[0-9]+\\]', '', 
-        row.names(jagsfit$BUGSoutput$summary)), invert=invert, ...)
+        row.names(x$BUGSoutput$summary)), invert=invert, ...)
     }
-    return(jagsfit$BUGSoutput$summary[rows,, drop=FALSE])
+    return(x$BUGSoutput$summary[rows,, drop=FALSE])
   }
