20a7007f28d7605501f55f875a448d03b32e5f36
johnbaums <john.baumgartner@live.com>
2012-06-13 12:30:35 +1000

    changed jagsresults() significantly, adding new arguments to improve usability.

--- R/jagsresults.R
+++ R/jagsresults.R
@@ -1,18 +1,20 @@
 jagsresults <-
-function(x, param, exact=TRUE) {
-  if (length(param > 1)) {
-pat <- vector()
-    for (i in 1:length(param)) {
-      if (exact) {
-        pat[i] <- paste('^', param[i], '\\[|^', param[i], '$', sep='')
-      } else {
-        pat[i] <- param[i]
-      }
-}
-pat <- paste(c(pat), collapse='|')
-}else {
-pat <- paste('^', param, '\\[|^', param, '$', sep='')
-}
-x$BUGSoutput$summary[grep(pat, row.names(x$BUGSoutput$summary)),]
-}
-
+  function(x, params, not=FALSE, exact=TRUE, regex=FALSE, ...) {
+    if (!regex) {
+      rows <- unique(c(sapply(params, function(z) {
+        if (exact) {
+          grep(paste('^', z, '$', sep=''), 
+            gsub('\\[[0-9]+\\]', '', row.names(jagsfit$BUGSoutput$summary)))
+        } else {
+          grep(z, row.names(jagsfit$BUGSoutput$summary))
+        }
+      }), recursive=TRUE))
+    } else {
+      rows <- grep(params, gsub('\\[[0-9]+\\]', '', 
+        row.names(jagsfit$BUGSoutput$summary)), ...)
+    }
+    if (not) {
+      return(jagsfit$BUGSoutput$summary[-rows,, drop=FALSE])
+    } 
+    return(jagsfit$BUGSoutput$summary[rows,, drop=FALSE])
+  }
