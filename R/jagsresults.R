459b92834c30ead9531d0ad55b9d55165287a2f1
johnbaums <john.baumgartner@live.com>
2012-09-02 23:29:47 +1000

    jagsresults updated for compatibility with mcmc.list objects created applying "coda.samples" to a "jags" object.

--- R/jagsresults.R
+++ R/jagsresults.R
@@ -1,18 +1,38 @@
-jagsresults <-
-  function(x, params, invert=FALSE, exact=TRUE, regex=FALSE, ...) {
+jagsresults <- function (x, params, invert = FALSE, exact = TRUE, regex = FALSE, 
+               ...) 
+{
+  if (is(x, 'rjags')) {
     if (!regex) {
-      rows <- unique(c(sapply(params, function(z) {
-        if (exact) {
-          grep(paste('^', z, '$', sep=''), 
-            gsub('\\[[0-9]+\\]', '', row.names(x$BUGSoutput$summary)), 
-               invert=invert)
-        } else {
-          grep(z, row.names(x$BUGSoutput$summary), invert=invert)
-        }
-      }), recursive=TRUE))
-    } else {
-      rows <- grep(params, gsub('\\[[0-9]+\\]', '', 
-        row.names(x$BUGSoutput$summary)), invert=invert, ...)
+      params <- paste(params, collapse = "|")
+      if (exact) 
+        params <- paste("^", gsub("\\|", "\\$\\|\\^", params), 
+                        "$", sep = "")
+      rows <- grep(params, gsub("\\[[0-9]+\\]", "", row.names(x$BUGSoutput$summary)), 
+                   invert = invert)
     }
-    return(x$BUGSoutput$summary[rows,, drop=FALSE])
-  }
+    else {
+      rows <- grep(params, gsub("\\[[0-9]+\\]", "", row.names(x$BUGSoutput$summary)), 
+                   invert = invert, ...)
+    }
+    return(x$BUGSoutput$summary[rows, , drop = FALSE])
+  } else if (is(x, 'mcmc.list')) {
+    
+    if (!regex) {
+      params <- paste(params, collapse = "|")
+      if (exact) 
+        params <- paste("^", gsub("\\|", "\\$\\|\\^", params), 
+                        "$", sep = "")
+      rows <- grep(params, gsub("\\[[0-9]+\\]", "", colnames(x[[1]])), 
+                   invert = invert)
+    }
+    else {
+      rows <- grep(params, gsub("\\[[0-9]+\\]", "", colnames(x[[1]])), 
+                   invert = invert, ...)
+    }
+    SUMM <- t(apply(do.call(rbind, x), 2, function(z) {
+      c(mean=mean(z), sd=sd(z), quantile(z, c(0.025, 0.25, 0.5, 0.75, 0.975)))
+    }))
+    return(SUMM[rows, , drop = FALSE])
+    
+  } else stop('x must be either an "rjags" object or an "mcmc.list" object.')
+}
