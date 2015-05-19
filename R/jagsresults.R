c3e5d49d35732072124bebfa2aced83a4ed5a3b3
John Baumgartner <johnbaums@gmail.com>
2015-05-19 23:55:24 +1000

    Correction to previous commit - fix for #1

--- R/jagsresults.R
+++ R/jagsresults.R
@@ -11,8 +11,7 @@ jagsresults <- function (x, params, invert = FALSE, exact = TRUE, regex = FALSE,
                    invert = invert)
     }
     else {
-      rows <- grep(params, gsub("\\[[0-9,]+\\]", "", row.names(x$BUGSoutput$summary)), 
-                   invert = invert, ...)
+      rows <- grep(params, row.names(x$BUGSoutput$summary), invert = invert, ...)
     }
     return(x$BUGSoutput$summary[rows, , drop = FALSE])
   } else if (is(x, "stanfit")) {
@@ -26,8 +25,7 @@ jagsresults <- function (x, params, invert = FALSE, exact = TRUE, regex = FALSE,
                    invert = invert)
     }
     else {
-      rows <- grep(params, gsub("\\[[0-9,]+\\]", "", row.names(x$summary)), 
-                   invert = invert, ...)
+      rows <- grep(params, row.names(x$summary), invert = invert, ...)
     }
     return(x$summary[rows, , drop = FALSE])
   } else if (is(x, 'mcmc.list')) {
@@ -41,8 +39,7 @@ jagsresults <- function (x, params, invert = FALSE, exact = TRUE, regex = FALSE,
                    invert = invert)
     }
     else {
-      rows <- grep(params, gsub("\\[[0-9,]+\\]", "", colnames(x[[1]])), 
-                   invert = invert, ...)
+      rows <- grep(params, colnames(x[[1]]), invert = invert, ...)
     }
     SUMM <- t(apply(do.call(rbind, x), 2, function(z) {
       c(mean=mean(z), sd=sd(z), quantile(z, c(0.025, 0.25, 0.5, 0.75, 0.975)))
