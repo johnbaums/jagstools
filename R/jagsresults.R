1b9ec666c7051600aa98f95e5d882f83f3d56903
johnbaums <johnbaums@gmail.com>
2016-02-21 17:11:32 +1100

    Remove arg 'exact', and add '...' to rearray - exact was redundant, since regex does the job... we can assume exact when regex=FALSE. - docs updated/clarified. - minor loss functionality because when 'regex' is TRUE, 'params' can no longer be a  vector. Will probably implement this.

--- R/jagsresults.R
+++ R/jagsresults.R
@@ -1,43 +1,34 @@
 #' Print results for specified rjags or mcmc.list parameters
 #' 
 #' Prints results for \code{rjags} or \code{mcmc.list} parameters, which are 
-#' specified with a regular expression. Pattern matching can be exact or 
-#' approximate.
+#' specified with a regular expression, or by exact name.
 #' 
 #' @param x The \code{rjags}, \code{rjags.parallel}, or \code{mcmc.list} object
 #'   for which results will be printed.
-#' @param params Character vector. The parameters for which results will be 
-#'   printed (unless \code{invert} is \code{FALSE}, in which case results for 
-#'   all parameters other than those given in \code{params} will be returned).
-#'   If \code{exact} is \code{FALSE}, then parameters whose names contain any of
-#'   \code{params} will be returned. If \code{exact} is \code{TRUE} only those
-#'   parameters that match \code{params} exactly will be returned. If 
+#' @param params Character vector or a regular expression pattern. The 
+#'   parameters for which results will be printed (unless \code{invert} is
+#'   \code{FALSE}, in which case results for all parameters other than those
+#'   given in \code{params} will be returned). If \code{regex} is \code{FALSE},
+#'   only those parameters that match \code{params} exactly will be returned. If 
 #'   \code{regex} is \code{TRUE}, \code{param} should be a character string 
-#'   giving the pattern to be matched.
-#' @param invert Logical. If \code{invert} is \code{TRUE}, only those parameters
-#'   that do not match elements of \code{params} will be returned.
-#' @param exact Logical. If \code{exact} is \code{TRUE}, only parameters whose 
-#'   names match elements of \code{param} exactly will be returned. If 
-#'   \code{exact = FALSE}, parameters that have names containing the strings 
-#'   given by \code{param} will be returned. Ignored if \code{regex} is 
-#'   \code{TRUE}.
+#'   giving the regular expression pattern to be matched.
 #' @param regex Logical. If \code{regex} is \code{TRUE}, then \code{param} is 
 #'   expected to be a single string giving a text pattern to be matched. 
 #'   Parameters with names matching the pattern will be returned (unless 
 #'   \code{invert} is \code{TRUE}, which results in all parameters that do not 
 #'   match the pattern being returned). Text pattern matching uses regular 
 #'   expressions (\code{\link{regex}}).
+#' @param invert Logical. If \code{invert} is \code{TRUE}, only those parameters
+#'   that do not match elements of \code{params} will be returned.
 #' @param probs A numeric vector of probabilities within range [0, 1],
 #'   representing the sample quantiles to be calculated and returned.
-#' @param ... Additional arguments accepted by grep, e.g. \code{perl} is 
-#'   \code{TRUE}, for example to allow look-around pattern matching.
+#' @param ... Additional arguments accepted by \code{\link{grep}}, e.g. 
+#'   \code{perl=TRUE}, to allow look-around pattern matching.
 #' @return A matrix with one row for each parameter that matches \code{param}, 
 #'   and one column for each of \code{mean}, \code{sd}, percentiles \code{2.5}, 
 #'   \code{25}, \code{50}, \code{75}, and \code{97.5}. In addition, if \code{x} 
 #'   is an \code{rjags} object, columns for \code{Rhat} and \code{neff} are 
 #'   returned.
-#' @author John Baumgartner, \email{johnbaums@@gmail.com}
-#' @author James Camac, \email{james.camac@@gmail.com}
 #' @seealso \code{\link{rhats}} for simplified rhat output, 
 #'   \code{\link{rearray}} for recovering array structure of vector-valued 
 #'   parameters.
@@ -83,25 +74,29 @@
 #' # model summary
 #' jagsfit
 #' 
-#' # results for beta.rain only
+#' # Results for beta.rain only
 #' jagsresults(x=jagsfit, param='beta.rain')
 #' 
-#' # results for 'a' and 'sd' only
+#' # Results for 'a' and 'sd' only
 #' jagsresults(x=jagsfit, param=c('a', 'sd'))
 #' jagsresults(x=jagsfit, param=c('a', 'sd'), 
 #'             probs=c(0.01, 0.025, 0.1, 0.25, 0.5, 0.75, 0.9, 0.975))
 #' 
-#' # results for all parameters including the string 'beta'
-#' jagsresults(x=jagsfit, param='beta', exact=FALSE)
+#' # Results for all parameters including the string 'beta'
+#' jagsresults(x=jagsfit, param='beta', regex=TRUE)
 #' 
-#' # results for all parameters not including the string 'beta'
-#' jagsresults(x=jagsfit, param='beta', exact=FALSE, invert=TRUE)
+#' # Results for all parameters not including the string 'beta'
+#' jagsresults(x=jagsfit, param='beta', regex=TRUE, invert=TRUE)
 #' 
-#' # results for all parameters beginning with 'b' or including 'sd'.
+#' # Note that the above is NOT equivalent to the following, which returns all
+#' # parameters that are not EXACTLY equal to 'beta'.
+#' jagsresults(x=jagsfit, param='beta', invert=TRUE)
+#' 
+#' # Results for all parameters beginning with 'b' or including 'sd'.
 #' jagsresults(x=jagsfit, param='^b|sd', regex=TRUE)
 #' 
-#' # results for all parameters not beginning with 'beta'.
-#' # note this is equivalent to using param='^beta' with invert=TRUE and regex=TRUE
+#' # Results for all parameters not beginning with 'beta'.
+#' # This is equivalent to using param='^beta' with invert=TRUE and regex=TRUE
 #' jagsresults(x=jagsfit, param='^(?!beta)', regex=TRUE, perl=TRUE)
 #' 
 #' ## mcmc.list example
@@ -112,21 +107,23 @@
 #'                setdiff(simgrowth$parameters.to.save, 'deviance'), 1000)
 #' is(simgrowth_mcmclist)
 #' jagsresults(simgrowth_mcmclist, c('b[3]', 'log.r[2,1]'))
-#' jagsresults(simgrowth_mcmclist, c('lambda[3,1]', 'sd.'), exact=FALSE)
+#' jagsresults(simgrowth_mcmclist, c('b[3]', 'log.r[2,1]'), invert=TRUE)
+#' jagsresults(simgrowth_mcmclist, c('lambda[3,1]', 'sd.b'))
 #' jagsresults(simgrowth_mcmclist, c('lambda\\[3,1\\]|sd\\.'), regex=TRUE)
-jagsresults <- function(x, params, invert = FALSE, exact = TRUE, regex = FALSE, 
+jagsresults <- function(x, params, regex=FALSE, invert=FALSE, 
                         probs=c(0.025, 0.25, 0.5, 0.75, 0.975), ...) {
   if(!regex) {
     params <- paste0(gsub('(?=\\.|\\[|\\])', '\\1\\\\', params, perl=TRUE),
                      '(\\[.*\\])?', collapse='|')
-    if (exact) params <- paste("^", gsub("\\|", "$|^", params), sep = "")
-  } else {
-    if(exact) warning('exact=TRUE ignored when regex=TRUE')
+    params <- paste("^", gsub("\\|", "$|^", params), '$', sep = "")
+  } else if(length(params) > 1) {
+    stop("If 'regex' is TRUE, 'params' must be a single regex string.",
+         call.=FALSE)
   }
   if(any(is(x) %in% c('rjags.parallel', 'rjags'))) {
     nm <- dimnames(x$BUGSoutput$sims.array)[[3]]
     i <- grep(params, nm, invert=invert, ...)
-    if(length(i) == 0) stop('No parameters match params', call.=FALSE)
+    if(length(i) == 0) stop("No parameters match 'params'", call.=FALSE)
     samp <- x$BUGSoutput$sims.array[, , i, drop=FALSE] 
     rhat_neff <- x$BUGSoutput$summary[i, c('Rhat', 'n.eff'), drop=FALSE]
     return(cbind(t(apply(
@@ -135,11 +132,11 @@ jagsresults <- function(x, params, invert = FALSE, exact = TRUE, regex = FALSE,
   } else if(any(is(x)=='mcmc.list')) {
     nm <- colnames(x[[1]])
     i <- grep(params, nm, invert=invert, ...)
-    if(length(i) == 0) stop('No parameters match params', call.=FALSE)
+    if(length(i) == 0) stop("No parameters match 'params'", call.=FALSE)
     t(apply(do.call(rbind, x), 2, function(z) {
       c(mean=mean(z), sd=sd(z), quantile(z, probs))
     }))[i, , drop=FALSE]
   } else {
-    stop('x must be an mcmc.list or rjags  object.')
+    stop("x must be an 'mcmc.list' or 'rjags'  object.")
   }
 }
--- R/rearray.R
+++ R/rearray.R
@@ -5,59 +5,57 @@
 #' 
 #' @param x The \code{rjags}, \code{rjags.parallel}, or \code{mcmc.list} object
 #'   for which array structure will be recovered.
-#' @param param Character vector. The multidimensional parameters that will be 
-#'   given an array structure. Default is \code{'all'}, which returns arrays for
-#'   all multidimensional parameters in \code{x}.
+#' @param param Character vector of exact names of multidimensional parameters 
+#'   (if \code{regex} is \code{FALSE}, the default), or a regular expression
+#'   pattern that will be compared to all available multidimensional parameters
+#'   (if \code{regex} is \code{TRUE}). If left unspecified, arrays will be
+#'   returned for all multidimensional parameters in \code{x}.
 #' @param fields A vector (character or integer) that indicates the summary 
-#'   fields that should be included as array slices. Valid fields are: 'mean', 
-#'   'sd', '2.5\%', '25\%', '50\%', '75\%', '97.5\%', 'Rhat', and 'n.eff'. If 
-#'   supplied as an integer vector, numbers should indicate the column numbers 
-#'   (i.e. positions in the series given above). Note that 'Rhat' and 'n.eff' 
-#'   are only available if \code{x} is an \code{rjags} object. Default is 
-#'   \code{'all'}, which returns all available fields as array slices.
-#' @param exact Logical. If \code{exact} is \code{TRUE}, only parameters whose 
-#'   names match elements of \code{param} exactly will be returned. If 
-#'   \code{exact = FALSE}, parameters that have names containing the strings 
-#'   given by \code{param} will be returned. Ignored if \code{regex} is 
-#'   \code{TRUE}.
-#' @param regex Logical. If \code{regex} is \code{TRUE}, then \code{param} is 
-#'   expected to be a single string giving a text pattern to be matched. 
-#'   Parameters with names matching the pattern will be returned (unless 
-#'   \code{invert} is \code{TRUE}, which results in all parameters that do not 
-#'   match the pattern being returned). Text pattern matching uses regular 
-#'   expressions (\code{\link{regex}}).
+#'   fields that should be included as array slices. Valid fields are:
+#'   \code{'mean'}, \code{'sd'}, \code{'Rhat'}, \code{'n.eff'}, and a range of
+#'   quantiles. By default, these quantiles include \code{'2.5\%'}, 
+#'   \code{'25\%'}, \code{'50\%'}, \code{'75\%'}, and \code{'97.5\%'}, but if a
+#'   vector of alternative quantiles is passed via \code{probs} (see examples),
+#'   then those quantiles' names may be used. Note that 'Rhat' and 'n.eff' are
+#'   only available if \code{x} is an \code{rjags} or \code{rjags.parallel}
+#'   object. If left unspecified, all available fields are returned.
+#' @param regex Logical. If \code{regex} is \code{TRUE}, then \code{param} 
+#'   should be a  single string giving a text pattern to be matched. Parameters 
+#'   with names matching the pattern will be returned (unless the match is
+#'   inverted by passing \code{invert=TRUE} via \code{...}, which results in all
+#'   parameters that do \emph{not} match the pattern being returned). Text
+#'   pattern matching uses regular expressions (\code{\link{regex}}).
+#' @param ... Additional arguments passed to \code{\link{jagsresults}}.
 #' @return A list of arrays (one per multidimensional model parameter specified
 #'   in \code{param}) containing node summaries for the requested \code{fields}.
-#'   Fields (e.g. mean, sd, Rhat, etc.) will be included as the last
+#'   Fields (e.g. 'mean', 'sd', 'Rhat', etc.) will be included as the final
 #'   (right-most) dimension.
-#' @author John Baumgartner, \email{johnbaums@@gmail.com}
 #' @seealso \code{\link{jagsresults}}
 #' @export
 #' @examples
 #' data(simgrowth)
 #' a <- rearray(simgrowth)
 #' str(a)
-#' a2 <- rearray(simgrowth, param='lambda', fields=c('mean', '50\%', 'n.eff'))
+#' a2 <- rearray(simgrowth, param='lambda', fields=c('mean', '50%', 'n.eff'))
 #' str(a2)
-#' # or...
-#' a2 <- rearray(simgrowth, param='lambda', fields=c(1, 2, 9))
-#' str(a2)
-rearray <- function(x, param='all', fields='all', regex = TRUE, exact=FALSE) {
-  if(identical(param, 'all')) param <- '.*'
-  results <- jagsresults(x, param, regex=regex, exact=exact)
-  if(!any(sapply(x$BUGSoutput$sims.list, function(x) length(dim(x)) > 2))) {
+#' a3 <- rearray(simgrowth, param='log.r', probs=seq(0, 1, 0.1))
+#' str(a3)
+rearray <- function(x, param, fields, regex=FALSE, ...) {
+  if(missing(param)) param <- '.*\\[\\d+(,\\d+)+\\]'
+  results <- jagsresults(x, param, regex=regex, ...)
+  if(!length(results))
     stop(sprintf('No arrays found in object %s', deparse(substitute(x))))
-  }
-  if(identical(fields, 'all')) fields <- colnames(results)
-  if(!(all(fields %in% colnames(results))) & !(all(fields %in% seq_len(ncol(results))))) {
-    stop(sprintf("fields must be either 'all', or a character vector of jags ',
-                 'summary column names, which may include: %s", 
+  if(missing(fields)) fields <- colnames(results)
+  if(!(all(fields %in% colnames(results)))) {
+    stop(sprintf("fields must be unspecified, or a character vector of ",
+                 "summary column names, which may include: %s", 
                  toString(colnames(results))))
   }
   results <- results[, fields, drop=FALSE]
-  splt_results <- split(as.data.frame(results), sapply(strsplit(row.names(results), '\\[|,|\\]'), function(x) x[1]))
-  # row.matches adapted from https://stat.ethz.ch/pipermail/r-help/2000-July/007393.html
-  row.matches <- function(y, X) {
+  splt_results <- split(as.data.frame(results), sapply(
+    strsplit(row.names(results), '\\[|,|\\]'), function(x) x[1]))
+  # row_matches adapted from https://stat.ethz.ch/pipermail/r-help/2000-July/007393.html
+  row_matches <- function(y, X) {
     i <- seq(nrow(X))
     j <- 0
     while(length(i) && (j <- j + 1) <= ncol(X)) 
@@ -70,7 +68,7 @@ rearray <- function(x, param='all', fields='all', regex = TRUE, exact=FALSE) {
     a <- array(dim=c(apply(ind, 2, max), ncol(x)))
     arrind <- arrayInd(seq_along(a), dim(a))
     invisible(lapply(1:nrow(ind), function(i) {
-      a[row.matches(ind[i,], arrind[, -ncol(arrind)])] <<- unlist(x[i,])
+      a[row_matches(ind[i,], arrind[, -ncol(arrind)])] <<- unlist(x[i,])
     }))   
     dimnames(a)[[length(dim(a))]] <- as.list(colnames(x))
     return(drop(a))
